c673bf299a7659824f3fd41f2d67e61737a03f3b
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-10-17 01:43:00 +0800

    Add some dummy data.

---
+++ DummyData/JoinsBooksApproves.sql
@@ -0,0 +1,56 @@
+insert into Joins (eid, time, date, floor, room) values (215, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (123, '11:00:00', '2021-10-16', 1, 3);
+insert into Joins (eid, time, date, floor, room) values (74, '14:00:00', '2021-10-20', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (24, '13:00:00', '2021-10-19', 3, 5);
+insert into Joins (eid, time, date, floor, room) values (216, '10:00:00', '2021-10-16', 4, 5);
+insert into Joins (eid, time, date, floor, room) values (65, '15:00:00', '2021-10-22', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (34, '16:00:00', '2021-10-20', 4, 3);
+insert into Joins (eid, time, date, floor, room) values (54, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (45, '13:00:00', '2021-10-22', 1, 2);
+insert into Joins (eid, time, date, floor, room) values (64, '16:00:00', '2021-10-23', 3, 1);
+
+insert into Joins (eid, time, date, floor, room) values (45, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (13, '11:00:00', '2021-10-16', 1, 3);
+insert into Joins (eid, time, date, floor, room) values (174, '14:00:00', '2021-10-20', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (124, '13:00:00', '2021-10-19', 3, 5);
+insert into Joins (eid, time, date, floor, room) values (116, '10:00:00', '2021-10-16', 4, 5);
+insert into Joins (eid, time, date, floor, room) values (165, '15:00:00', '2021-10-22', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (134, '16:00:00', '2021-10-20', 4, 3);
+insert into Joins (eid, time, date, floor, room) values (154, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (145, '13:00:00', '2021-10-22', 1, 2);
+insert into Joins (eid, time, date, floor, room) values (164, '16:00:00', '2021-10-23', 3, 1);
+
+insert into Joins (eid, time, date, floor, room) values (115, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (23, '11:00:00', '2021-10-16', 1, 3);
+insert into Joins (eid, time, date, floor, room) values (274, '14:00:00', '2021-10-20', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (224, '13:00:00', '2021-10-19', 3, 5);
+insert into Joins (eid, time, date, floor, room) values (119, '10:00:00', '2021-10-16', 4, 5);
+insert into Joins (eid, time, date, floor, room) values (265, '15:00:00', '2021-10-22', 1, 1);
+insert into Joins (eid, time, date, floor, room) values (234, '16:00:00', '2021-10-20', 4, 3);
+insert into Joins (eid, time, date, floor, room) values (254, '10:00:00', '2021-10-17', 5, 5);
+insert into Joins (eid, time, date, floor, room) values (245, '13:00:00', '2021-10-22', 1, 2);
+insert into Joins (eid, time, date, floor, room) values (264, '16:00:00', '2021-10-23', 3, 1);
+
+
+insert into Books (eid, time, date, floor, room) values (251, '10:00:00', '2021-10-17', 5, 5);
+insert into Books (eid, time, date, floor, room) values (271, '11:00:00', '2021-10-16', 1, 3);
+insert into Books (eid, time, date, floor, room) values (293, '14:00:00', '2021-10-20', 1, 1);
+insert into Books (eid, time, date, floor, room) values (264, '13:00:00', '2021-10-19', 3, 5);
+insert into Books (eid, time, date, floor, room) values (261, '10:00:00', '2021-10-16', 4, 5);
+insert into Books (eid, time, date, floor, room) values (219, '15:00:00', '2021-10-22', 1, 1);
+insert into Books (eid, time, date, floor, room) values (259, '16:00:00', '2021-10-20', 4, 3);
+insert into Books (eid, time, date, floor, room) values (257, '10:00:00', '2021-10-17', 5, 5);
+insert into Books (eid, time, date, floor, room) values (271, '13:00:00', '2021-10-22', 1, 2);
+insert into Books (eid, time, date, floor, room) values (299, '16:00:00', '2021-10-23', 3, 1);
+
+
+insert into Approves (eid, time, date, floor, room) values (291, '10:00:00', '2021-10-17', 5, 5);
+insert into Approves (eid, time, date, floor, room) values (291, '11:00:00', '2021-10-16', 1, 3);
+insert into Approves (eid, time, date, floor, room) values (293, '14:00:00', '2021-10-20', 1, 1);
+insert into Approves (eid, time, date, floor, room) values (294, '13:00:00', '2021-10-19', 3, 5);
+insert into Approves (eid, time, date, floor, room) values (291, '10:00:00', '2021-10-16', 4, 5);
+insert into Approves (eid, time, date, floor, room) values (299, '15:00:00', '2021-10-22', 1, 1);
+insert into Approves (eid, time, date, floor, room) values (299, '16:00:00', '2021-10-20', 4, 3);
+insert into Approves (eid, time, date, floor, room) values (297, '10:00:00', '2021-10-17', 5, 5);
+insert into Approves (eid, time, date, floor, room) values (291, '13:00:00', '2021-10-22', 1, 2);
+insert into Approves (eid, time, date, floor, room) values (299, '16:00:00', '2021-10-23', 3, 1);
