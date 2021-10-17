b668716ce5501d1f7117ea494951cb7db63beb9a
STAung07 <swann1999@gmail.com>
2021-10-17 17:04:08 +0800

    Merge with upstream

---
+++ routines.sql
@@ -0,0 +1,17 @@
+-- BASIC
+-- change capacity
+CREATE OR REPLACE PROCEDURE change_capacity(
+    IN floor_number INT, IN room_number INT, IN capacity INT, IN date DATE
+) AS $$
+$$ LANGUAGE sql;
+
+-- add_employee
+
+-- remove_employee
+
+-- CORE
+-- search_room
+
+-- book_room
+
+-- unbook_room
