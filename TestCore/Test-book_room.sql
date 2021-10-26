7d574cda03e37217c7338b44ebca3132efa1d726
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-26 23:20:50 +0800

    Add more tests for book_room

--- TestCore/Test-book_room.sql
+++ TestCore/Test-book_room.sql
@@ -3,6 +3,12 @@ CALL book_room(4, 1, '2022-10-26', '16:00:00', '17:00:00', 298); -- manager
 CALL book_room(4, 1, '2022-10-26', '17:00:00', '18:00:00', 252); -- senior
 CALL book_room(2, 2, '2022-10-26', '18:00:00', '19:00:00', 298); -- manager
 
+-- EXPECTED PASS
+CALL book_room(4, 1, '2022-10-28', '17:00:00', '18:00:00', 298); -- manager
+
+-- EXPECTED FAIL - cannot book all slots
+CALL book_room(4, 1, '2022-10-28', '16:00:00', '19:00:00', 252); -- manager
+
 -- EXPECTED FAIL BOOK PAST  
 CALL book_room(4, 1, '2021-10-02', '16:00:00', '18:00:00', 298);
 CALL book_room(4, 1, '2021-10-02', '18:00:00', '19:00:00', 275);
