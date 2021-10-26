98ad18e8adcc52088f9844ade66ae188b58b9978
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2021-10-26 23:19:57 +0800

    Clean data

--- DummyData/Updates.sql
+++ DummyData/Updates.sql
@@ -1,17 +1,30 @@
-insert into Updates (eid, date, new_cap, floor, room) values (295, '2019-10-15', 10, 5, 1);
-insert into Updates (eid, date, new_cap, floor, room) values (300, '2019-10-15', 16, 5, 3);
-insert into Updates (eid, date, new_cap, floor, room) values (300, '2019-10-15', 10, 5, 2);
-insert into Updates (eid, date, new_cap, floor, room) values (293, '2019-10-15', 12, 1, 5);
-insert into Updates (eid, date, new_cap, floor, room) values (293, '2019-10-15', 12, 1, 6);
+insert into Updates (eid, date, new_cap, floor, room) values (301, '2019-10-15', 11, 1, 1);
+insert into Updates (eid, date, new_cap, floor, room) values (301, '2019-10-15', 15, 1, 2);
+insert into Updates (eid, date, new_cap, floor, room) values (302, '2019-10-15', 16, 1, 3);
+insert into Updates (eid, date, new_cap, floor, room) values (297, '2019-10-15', 21, 1, 4);
+insert into Updates (eid, date, new_cap, floor, room) values (296, '2019-10-15', 25, 1, 5);
+insert into Updates (eid, date, new_cap, floor, room) values (296, '2019-10-15', 12, 1, 6);
 insert into Updates (eid, date, new_cap, floor, room) values (292, '2019-10-15', 10, 2, 1);
 insert into Updates (eid, date, new_cap, floor, room) values (292, '2019-10-15', 20, 2, 2);
 insert into Updates (eid, date, new_cap, floor, room) values (292, '2019-10-15', 16, 2, 3);
-insert into Updates (eid, date, new_cap, floor, room) values (297, '2019-10-15', 20, 4, 1);
+insert into Updates (eid, date, new_cap, floor, room) values (305, '2019-10-15', 20, 2, 4);
+insert into Updates (eid, date, new_cap, floor, room) values (305, '2019-10-15', 16, 2, 5);
+insert into Updates (eid, date, new_cap, floor, room) values (306, '2019-10-15', 30, 3, 1);
+insert into Updates (eid, date, new_cap, floor, room) values (306, '2019-10-15', 20, 3, 2);
+insert into Updates (eid, date, new_cap, floor, room) values (306, '2019-10-15', 10, 3, 3);
+insert into Updates (eid, date, new_cap, floor, room) values (307, '2019-10-15', 20, 3, 4);
+insert into Updates (eid, date, new_cap, floor, room) values (307, '2019-10-15', 16, 3, 5);
+insert into Updates (eid, date, new_cap, floor, room) values (298, '2019-10-15', 20, 4, 1);
 insert into Updates (eid, date, new_cap, floor, room) values (298, '2019-10-15', 14, 4, 2);
-insert into Updates (eid, date, new_cap, floor, room) values (294, '2019-10-15', 21, 4, 3);
-insert into Updates (eid, date, new_cap, floor, room) values (294, '2019-10-15', 8, 4, 4);
-insert into Updates (eid, date, new_cap, floor, room) values (294, '2019-10-15', 14, 4, 5);
-insert into Updates (eid, date, new_cap, floor, room) values (291, '2019-10-15', 50, 6, 1);
+insert into Updates (eid, date, new_cap, floor, room) values (299, '2019-10-15', 21, 4, 3);
+insert into Updates (eid, date, new_cap, floor, room) values (299, '2019-10-15', 8, 4, 4);
+insert into Updates (eid, date, new_cap, floor, room) values (299, '2019-10-15', 14, 4, 5);
+insert into Updates (eid, date, new_cap, floor, room) values (300, '2019-10-15', 10, 5, 1);
+insert into Updates (eid, date, new_cap, floor, room) values (300, '2019-10-15', 10, 5, 2);
+insert into Updates (eid, date, new_cap, floor, room) values (300, '2019-10-15', 16, 5, 3);
+insert into Updates (eid, date, new_cap, floor, room) values (295, '2019-10-15', 10, 5, 4);
+insert into Updates (eid, date, new_cap, floor, room) values (295, '2019-10-15', 16, 5, 5);
+insert into Updates (eid, date, new_cap, floor, room) values (293, '2019-10-15', 50, 6, 1);
 
 
 
